dbe09c295f2d84b38aeaf52af367345f0c8a9f7c
Matthew Riley Gray <matthew.riley.gray@hatboysoftware.com>
2024-09-09 14:22:16 -0500

    one time vote

--- mock-up/src/Voting.js
+++ mock-up/src/Voting.js
@@ -2,21 +2,24 @@ import { useState } from 'react';
 
 export default function Voting() {
     const [val, alterVal] = useState(0);
+    const [hasAnswered, willAnswer] = useState(false);
 
     function increase() {
         alterVal(val+1);
+        willAnswer(true);
     }
 
     function decrease() {
         alterVal(val-1);
+        willAnswer(true);
     }
     
 
     return(
         <div>
-            <button onClick={increase}>Upvote!</button>
+            <button onClick={increase} disabled={hasAnswered}>Upvote!</button>
             <p>{val}</p>
-            <button onClick={decrease}>Downvote!</button>
+            <button onClick={decrease} disabled={hasAnswered}>Downvote!</button>
         </div>
     );
 }
