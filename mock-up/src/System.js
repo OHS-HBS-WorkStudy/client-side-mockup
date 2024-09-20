91040f19c5bc93ff786fb4074f873140c399bdd4
UltraHispeed-Elite <62296749+UltraHispeed-Elite@users.noreply.github.com>
2024-09-20 15:36:06 -0500

    rewrite commit #3

--- mock-up/src/System.js
+++ mock-up/src/System.js
@@ -1,15 +1,24 @@
-import {useState, getContext} from 'react';
+import {useState, getContext, createContext} from 'react';
 
 import Title from './Title.js';
 import Display from './Display.js';
 import Navigator from './Navigator.js';
 
+export const AccountContext = createContext("none");
+export const AccountChangeContext = createContext(0);
+
 export default function System() {
+    const [account, accountChange] = useState("none");
+
     return(
-        <div>
-            <Title />
-            <Display />
-            <Navigator />
-        </div>
+        <AccountContext.Provider value={account}>
+            <AccountChangeContext.Provider value={accountChange}>
+                <div>
+                    <Title />
+                    <Display />
+                    <Navigator />
+                </div>
+            </AccountChangeContext.Provider>
+        </AccountContext.Provider>
     );
 }
--- mock-up/src/screens/Home.js
+++ mock-up/src/screens/Home.js
@@ -1,7 +1,42 @@
+import NewThread from "./NewThread";
+
+import CustomButton from "../CustomButton";
+
 export default function Home() {
+
+    function getQuestions() {
+        let num;
+        try {
+            num = Number(localStorage.getItem("questionCount"));
+        }catch(err) {
+            num = 0;
+            localStorage.setItem("questionCount", 0);
+        }
+
+        if(num > 0) {
+            let questionArray = [];
+            for (let i = 0; i < num; i++) {
+                let data = localStorage.getItem("question"+(i+1));
+                questionArray.push(JSON.parse(data));
+            }
+
+            return questionArray
+        }
+
+
+    }
+
+    let questions = getQuestions();
+    console.log(questions)
+
     return(
         <div>
             <h2>Home</h2>
+
+            {questions.map(question => 
+                <h1>{question.title}</h1>
+            )}
+
         </div>
     );
 }
